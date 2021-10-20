fffee7ffde57d5f159ad8144476e344aadb5e399
shubham1994s <kawleshubham05@gmail.com>
2021-10-20 18:02:47 +0530

    comit by shubham

--- PTCSURVEYCMS/Content/Script/SearchDatatableFamily.js
+++ PTCSURVEYCMS/Content/Script/SearchDatatableFamily.js
@@ -283,6 +283,8 @@ function show1() {
     document.getElementById('filterbyname').style.display = 'none';
     document.getElementById('filterbyProperty').style.display = 'none';
     document.getElementById('filter').style.display = 'block';
+    document.getElementById('firsttable').style.display = 'block';
+    document.getElementById('secondtable').style.display = 'none';
     FillPrabhagListNo();
     FillWardListNo();
     FillCSDate();
@@ -313,6 +315,9 @@ function show2() {
     document.getElementById('filterbyname').style.display = 'none';
     document.getElementById('filterbyProperty').style.display = 'none';
     document.getElementById('filter').style.display = 'block';
+    document.getElementById('firsttable').style.display = 'block';
+    document.getElementById('secondtable').style.display = 'none';
+    
 
     $("#OCNO").empty();
     $("#OCNO").append("<option value='' selected>मालमत्तेची सद्यस्तिथी</option>< option value = 'ALL' > Select All.</option >  <option value='Safe'>भिंती (सुस्थितीत)</option><option value='Danger'>भिंती (धोकदायक)</option> <option value='Safe2'>छप्पर (सुस्थितीत)</option> <option value='Danger2'>छप्पर (धोकदायक)</option><option value='Safe3'>काँलम (सुस्थितीत)</option><option value='Danger3'>काँलम (धोकदायक)</option>");
@@ -387,6 +392,8 @@ function show6() {
     document.getElementById('filterbyProperty').style.display = 'none';
     document.getElementById('filter').style.display = 'block';
     document.getElementById('firsttable').style.display = 'block';
+    document.getElementById('firsttable').style.display = 'block';
+    document.getElementById('secondtable').style.display = 'none';
 
     $("#OCNO").empty();
     $("#OCNO").append("<option value='' selected>मालमत्तेची सद्यस्तिथी</option>< option value = 'ALL' > Select All.</option >  <option value='Safe'>भिंती (सुस्थितीत)</option><option value='Danger'>भिंती (धोकदायक)</option> <option value='Safe2'>छप्पर (सुस्थितीत)</option> <option value='Danger2'>छप्पर (धोकदायक)</option><option value='Safe3'>काँलम (सुस्थितीत)</option><option value='Danger3'>काँलम (धोकदायक)</option>");
